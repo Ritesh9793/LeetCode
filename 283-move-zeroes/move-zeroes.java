b42abb5b020552e8b1541c4723db30b0f0db022d
Ritesh Gupta <65335183+Ritesh9793@users.noreply.github.com>
2025-09-18 04:29:48 +0530

    Time: 3 ms (20.20%) | Memory: 46.2 MB (54.99%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for(int i = 0; i <= nums.length-1; i++){
+            //agar nums[i] zero nahi hai aur nums[j] zero hai to dono ko swap kar dunga
+            if(nums[i] != 0 && nums[j]==0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+            //agar nums[j] zero nahi hai to agla element compare karlenge
+            if(nums[j]!=0){
+                j++;
+            }
+        }
+    }
+}
