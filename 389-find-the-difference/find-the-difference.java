3426b7f1023ff302b5f8c2e7b5e03df276c8705a
Ritesh Gupta <65335183+Ritesh9793@users.noreply.github.com>
2025-09-20 10:59:21 +0530

    Time: 1 ms (99.31%) | Memory: 41.9 MB (64.62%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -2,29 +2,29 @@ class Solution {
     public char findTheDifference(String s, String t) {
         int xor = 0;
         
-        // // s ke sabhi char me XOR kar liya 
-        // for (char c : s.toCharArray()) {
-        //     xor ^= c;
-        // }
-        
-        // // Ab t ke sabhi char me XOR kar liya 
-        // for (char c : t.toCharArray()) {
-        //     xor ^= c;
-        // }
-        
-        // // dono char ke difference ko char me explicit conversion ki help se return kara liya
-        // return (char) xor;
-
-
-        //s aur t dono par XOR kar liya
-        for (int i = 0; i < s.length(); i++) {
-            xor ^= s.charAt(i);
-            xor ^= t.charAt(i);
+        // s ke sabhi char me XOR kar liya 
+        for (char c : s.toCharArray()) {
+            xor ^= c;
         }
-
-        // t ke last char ko XOR kar leta hu (kyoki t 1 char extra length ka hai)
-        xor ^= t.charAt(t.length() - 1);
-
+        
+        // Ab t ke sabhi char me XOR kar liya 
+        for (char c : t.toCharArray()) {
+            xor ^= c;
+        }
+        
+        // dono char ke difference ko char me explicit conversion ki help se return kara liya
         return (char) xor;
+
+
+        // //s aur t dono par XOR kar liya
+        // for (int i = 0; i < s.length(); i++) {
+        //     xor ^= s.charAt(i);
+        //     xor ^= t.charAt(i);
+        // }
+
+        // // t ke last char ko XOR kar leta hu (kyoki t 1 char extra length ka hai)
+        // xor ^= t.charAt(t.length() - 1);
+
+        // return (char) xor;
     }
 }
