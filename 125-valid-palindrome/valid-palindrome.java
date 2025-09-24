7005a689c3b1677379d486d01e79720915bd34e3
Ritesh Gupta <65335183+Ritesh9793@users.noreply.github.com>
2025-09-24 11:21:53 +0530

    Time: 3 ms (70.61%) | Memory: 43.4 MB (56.83%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) return true;
+        // double pointer variable use kar liya aur dono me start/end ka index store kar liya
+        int left = 0, right = s.length() - 1;
+        
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
